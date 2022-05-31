813b0063188be9860ea5fbcbc44e795b80dc699c
Fathima Nizar <fathimaen@gmail.com>
2022-05-31 20:13:34 +0530

    New Git

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require('express'); 
+const path = require ('path'); 
+const cors = require('cors'); // part#2 point#7
+const bodyParser =require ('body-parser');//part#1 point#1
+const mongoose= require('mongoose');//part#1 point#2
+
+const app = express(); 
+
+
+mongoose.Promise=global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/Library',{
+    useNewUrlParser:true, 
+    useUnifiedTopology:true
+ });
+
+const nav= [
+    {
+        link:"/books",
+        title:"Books"
+    },
+    {
+        link:"/authors",
+        title:"Authors"
+    },
+    {
+        link:"/addbook",
+        title:"Add Book"
+    },
+    {
+        link:"/addauthor",
+        title:"Add Author"
+    }
+]
+
+const loginRouter = require('./src/routes/loginroute');
+const signupRouter = require('./src/routes/signuproute');
+const homeRouter = require('./src/routes/homerouter');//part#1 point#3
+const booksRouter = require('./src/routes/booksroute');
+const authorsRouter = require('./src/routes/authorsroute');
+
+
+app.set('views','./src/views'); 
+app.set('view engine','ejs'); 
+
+app.use(cors()); // part#2 point#7
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname , '/public'))); 
+
+app.use('/login',loginRouter); 
+app.use('/signup',signupRouter); 
+app.use('/home',homeRouter); 
+app.use('/books',booksRouter); 
+app.use('/authors',authorsRouter); 
+
+app.get('/',(req,res)=>{
+
+    res.render('index',{});
+    
+});
+
+app.listen(5000,()=>{
+    console.log("Server Ready on 5000");//part#1 point#5
+});
+
+
+
+
