41aa8f945211aac37108f33fab783883db9d6643
Fathima Nizar <fathimaen@gmail.com>
2022-06-04 23:19:39 +0530

    add useNewUrlParser and  useUnifiedTopology

--- src/model/BookModel.js
+++ src/model/BookModel.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/Library');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/Library', {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
 const Schema = mongoose.Schema;
 
 
